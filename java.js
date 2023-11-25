9fed85bb699f7e0c41a8a8b2c0b8a21357a36b55
SoliBlack <SolitaireBlack21@gmail.com>
2023-11-24 21:54:28 -0500

    Added more functions
    Added the following functions:
    1. checkWinner()
    2. checkDraw()
    3. resetGame()

--- java.js
+++ java.js
@@ -2,19 +2,26 @@ const gameBoard = new Array(9).fill(null);
 
 const playerOne = {
     name: "",
-    mark: "",
+    mark: ""
 };
 const playerTwo = {
     name: "",
-    mark: "",
+    mark: ""
 
 };
+
+const computerTwo = {
+    name: "Computer 2",
+    mark: ""
+}
 
 let player1 = "";
 let player2 = "";
 
 let computer1 = "";
 let computer2 = "";
+
+let playerTurn = "";
 
 //factory function to create the player
 const getPlayer = (pName, marker) => {
@@ -46,6 +53,8 @@ const numPlayers = (num) => {
         computer1 = getPlayer("Computer 1", "X");
         computer2 = getPlayer("Computer 2", "O");
         console.log(computer1.getStatus(), computer2.getStatus());
+        playerTurn = Math.random() < .5 ? computer1 : computer2;
+        console.log(`${playerTurn.getName()} goes first`);
 
     }
     else if(num === 1){
@@ -60,8 +69,18 @@ const numPlayers = (num) => {
         const player1 = getPlayer(playerOne.name, playerOne.mark);
 
         //To create the computer and assign the name and mark
-        computer2 = getPlayer("Computer 2", "O");
+        if (player1.getMark() === "X"){
+            computerTwo.mark = "O";
+        }
+        else{
+            computerTwo.mark = "X";
+        }
+
+        computer2 = getPlayer(computerTwo.name, computerTwo.mark);
         console.log(player1.getStatus(), computer2.getStatus());
+
+        playerTurn = Math.random() < .5 ? player1 : player2;
+        console.log(`${playerTurn.getName()} goes first`);
     }
     else if(num === 2){
         //To create the first player and assign the name and mark
@@ -89,6 +108,10 @@ const numPlayers = (num) => {
         console.log(playerTwo.mark);
         player2 = getPlayer(playerTwo.name, playerTwo.mark);
         console.log(player1.getStatus(), player2.getStatus());
+
+        playerTurn = Math.random() < .5 ? player1 : player2;
+        console.log(`${playerTurn.getName()} goes first`);
+    
     }
     else{
         console.log("Invalid number of players")
@@ -100,6 +123,10 @@ const makeMove = (player, position) => {
     if(gameBoard[position] === null && position >= 0 && position <= 8){
         gameBoard[position] = player.getMark();
         console.log(gameBoard);
+        checkWinner(player);
+        checkDraw();
+        playerTurn = player;
+
     }
     else{
         console.log("Invalid move");
@@ -107,4 +134,39 @@ const makeMove = (player, position) => {
 }
 // To call this command type makeMove(playerOne, 3) in the console.
 
+const checkWinner = (player) => {
+    if (gameBoard[0] === gameBoard[1] && gameBoard[1] === gameBoard[2] && gameBoard[0] !== null){
+        console.log(`${player.getName()} is the winner`);
+    }
+    else if(gameBoard[3] === gameBoard[4] && gameBoard[4] === gameBoard[5] && gameBoard[3] !== null){
+        console.log(`${player.getName()} is the winner`);
+    }
+    else if(gameBoard[6] === gameBoard[7] && gameBoard[7] === gameBoard[8] && gameBoard[6] !== null){
+        console.log(`${player.getName()} is the winner`);
+    }
+    else if(gameBoard[0] === gameBoard[3] && gameBoard[3] === gameBoard[6] && gameBoard[0] !== null){
+        console.log(`${player.getName()} is the winner`);
+    }
+    else if(gameBoard[1] === gameBoard[4] && gameBoard[4] === gameBoard[7] && gameBoard[1] !== null){
+        console.log(`${player.getName()} is the winner`);
+    }
+    else if(gameBoard[2] === gameBoard[5] && gameBoard[5] === gameBoard[8] && gameBoard[2] !== null){
+        console.log(`${player.getName()} is the winner`);
+    }
+    else if(gameBoard[0] === gameBoard[4] && gameBoard[4] === gameBoard[8] && gameBoard[0] !== null){
+        console.log(`${player.getName()} is the winner`);
+    }
+    else if(gameBoard[2] === gameBoard[4] && gameBoard[4] === gameBoard[6] && gameBoard[2] !== null){
+        console.log(`${player.getName()} is the winner`);
+    }
+    else if(gameBoard[0] !== null && gameBoard[1] !== null && gameBoard[2] !== null && gameBoard[3] !== null && gameBoard[4] !== null && gameBoard[5] !== null && gameBoard[6] !== null && gameBoard[7] !== null && gameBoard[8] !== null){
+        console.log("Draw");
+    }
+    else{
+        console.log("Game in progress")
+    }
+}
+const resetGame = () => {
+    gameBoard.fill(null);
+}
 
